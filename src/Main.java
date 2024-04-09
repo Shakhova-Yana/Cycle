c477db285259a3e2bba6ee61a6002080ba0db5cf
НафНафДурикович <yana88yana99@gmail.com>
2024-04-09 21:13:51 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         //Task1
         System.out.println("Task 1\n");
+        for (int q = 1; q <= 10; q++) {
+            System.out.println("Итерация цикла " + q);
+        }
     }
 }
