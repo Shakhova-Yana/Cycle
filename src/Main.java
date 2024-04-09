2f2bf1bb58caf468a4ce496bb618ce3e2979858c
НафНафДурикович <yana88yana99@gmail.com>
2024-04-09 21:33:15 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         //Task1
-        System.out.println("Task 1\n");
+        System.out.println("\nTask 1");
         for (int q = 1; q <= 10; q++) {
             System.out.println("Итерация цикла " + q);
+        }
+        //Task2
+        System.out.println("\nTask 2");
+        for (int r = 10; r >= 1; r--) {
+            System.out.println("Итерация цикла " + r);
         }
     }
 }
